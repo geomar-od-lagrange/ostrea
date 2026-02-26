3abdbf2ffcfdbf55aadbd4827d4fb04752e5768f
Willi Rath <wrath@geomar.de>
2026-02-26 17:37:55 +0100

    preproc: drop CDL/compress, add jupyter+pydap, MVP explore notebook

--- preproc/compress.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p input_comp/
-for f in input/**/*.nc; do
-    echo "${f}"
-    f_="$(basename "$f")"
-    nccopy -d1 -s "$f" "input_comp/${f_}";
-    echo "... done"
-done
